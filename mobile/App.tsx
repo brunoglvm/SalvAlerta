e74ca7684e95bd7e22da41de757ccf0d187edb47
teixeira-g <gabriel.txr00@gmail.com>
2024-12-05 11:06:59 -0300

    Add integration with react-native-maps

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Map, {Callout, Marker} from "react-native-maps";
 
+const coordinate = {
+  latitude: -12.969717954906338,
+  longitude: -38.51277131734317,
+}
+//,
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Map
+          style={StyleSheet.absoluteFill}
+          initialRegion={{
+            latitude: coordinate.latitude,
+            longitude: coordinate.longitude,
+            latitudeDelta: 0.005,
+            longitudeDelta: 0.005,
+          }}
+      >
+        <Marker coordinate={coordinate}>
+          <Callout>
+            <View>
+              <Text style={styles.title}>Hub Salvador</Text>
+              <Text style={styles.address}>Avenida da França</Text>
+            </View>
+          </Callout>
+        </Marker>
+      </Map>
     </View>
   );
 }
@@ -13,8 +34,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  callout:{
+    padding: 12,
+  },
+  title:{
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+  address:{
+    fontSize: 14,
+  }
 });
