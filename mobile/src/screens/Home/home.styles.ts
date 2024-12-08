f9e1b1159078770b1ca4a14153e797cc58e36a87
teixeira-g <gabriel.txr00@gmail.com>
2024-12-08 20:53:27 -0300

    Create lateral buttons and renamed files

---
+++ mobile/src/screens/Home/home.styles.ts
@@ -0,0 +1,35 @@
+import styled from "styled-components/native";
+import Map from "react-native-maps";
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const StyledMap = styled(Map)`
+  flex: 1;
+`;
+
+export const CalloutView = styled.View``;
+
+export const Title = styled.Text`
+  font-size: 16px;
+  font-weight: bold;
+`;
+
+export const Address = styled.Text`
+  font-size: 14px;
+`;
+
+export const LateralButtonsContainer = styled.View`
+    position: absolute;
+    left: 10px;
+    top: 50%;
+    transform: translateY(-200px);
+    justify-content: space-between;
+    height: 200px;
+`;
+
+export const LateralButtonWrapper = styled.View`
+    margin-bottom: 10px;
+`
+
