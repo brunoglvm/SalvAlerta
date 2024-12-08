ad15b7379b2119ef1d42191e6c413a93d11d5eee
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-12-08 12:13:45 -0300

    feat(mobile): add support for SVG files

--- mobile/src/screens/Start/index.tsx
+++ mobile/src/screens/Start/index.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { Button } from "react-native";
 import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "@/utils/types";
-import { Background, Container } from "./styles";
+import { Background, ButtonContainer, LogoContainer } from "./styles";
+
+import Logo from "@/assets/images/logo.svg";
 
 type StartScreenNavigationProp = StackNavigationProp<
   RootStackParamList,
@@ -16,9 +18,12 @@ type Props = {
 export default function Start({ navigation }: Props) {
   return (
     <Background source={require("../../assets/images/bg.jpg")}>
-      <Container>
+      <LogoContainer>
+        <Logo width={240} height={60} />
+      </LogoContainer>
+      <ButtonContainer>
         <Button title="Test" onPress={() => navigation.navigate("Home")} />
-      </Container>
+      </ButtonContainer>
     </Background>
   );
 }
--- mobile/src/screens/Start/styles.ts
+++ mobile/src/screens/Start/styles.ts
@@ -3,10 +3,18 @@ import { colors } from "@/styles/colors";
 
 export const Background = styled.ImageBackground`
   flex: 1;
-  justify-content: flex-end;
   background-color: ${colors.gray[800]};
 `;
 
-export const Container = styled.View`
-  padding: 20px;
+export const ButtonContainer = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+  align-items: center;
+  padding-bottom: 20px;
+`;
+
+export const LogoContainer = styled.View`
+  position: absolute;
+  top: 15%;
+  align-self: center;
 `;
